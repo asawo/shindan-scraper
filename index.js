15dcc362a64ad8484b4c21d89a0ac55c8d54e92e
asawo <asawo@mercari.com>
2020-08-26 11:22:20 +0900

    cleaned up dialogue block

--- index.js
+++ index.js
@@ -54,56 +54,16 @@ bot.command('/shindan', async ({ ack, say }) => {
 					text: {
 						type: 'mrkdwn',
 						text:
-							'*1.* ' +
-							shindanMap[0].title +
-							'\n`' +
-							shindanMap[0].link +
-							'`\n' +
-							'*2.* ' +
-							shindanMap[1].title +
-							'\n`' +
-							shindanMap[1].link +
-							'`\n' +
-							'*3.* ' +
-							shindanMap[2].title +
-							'\n`' +
-							shindanMap[2].link +
-							'`\n' +
-							'*4.* ' +
-							shindanMap[3].title +
-							'\n`' +
-							shindanMap[3].link +
-							'`\n' +
-							'*5.* ' +
-							shindanMap[4].title +
-							'\n`' +
-							shindanMap[4].link +
-							'`\n' +
-							'*6.* ' +
-							shindanMap[5].title +
-							'\n`' +
-							shindanMap[5].link +
-							'`\n' +
-							'*7.* ' +
-							shindanMap[6].title +
-							'\n`' +
-							shindanMap[6].link +
-							'`\n' +
-							'*8.* ' +
-							shindanMap[7].title +
-							'\n`' +
-							shindanMap[7].link +
-							'`\n' +
-							'*9.* ' +
-							shindanMap[8].title +
-							'\n`' +
-							shindanMap[8].link +
-							'`\n' +
-							'*10.* ' +
-							shindanMap[9].title +
-							'\n`' +
-							shindanMap[9].link +
-							'`\n',
+							`*1.* ${shindanMap[0].title} \n \`${shindanMap[0].link}\`\n` +
+							`*2.* ${shindanMap[1].title} \n \`${shindanMap[1].link}\`\n` +
+							`*3.* ${shindanMap[2].title} \n \`${shindanMap[2].link}\`\n` +
+							`*4.* ${shindanMap[3].title} \n \`${shindanMap[3].link}\`\n` +
+							`*5.* ${shindanMap[4].title} \n \`${shindanMap[4].link}\`\n` +
+							`*6.* ${shindanMap[5].title} \n \`${shindanMap[5].link}\`\n` +
+							`*7.* ${shindanMap[6].title} \n \`${shindanMap[6].link}\`\n` +
+							`*8.* ${shindanMap[7].title} \n \`${shindanMap[7].link}\`\n` +
+							`*9.* ${shindanMap[8].title} \n \`${shindanMap[8].link}\`\n` +
+							`*10.* ${shindanMap[9].title} \n \`${shindanMap[9].link}\`\n`,
 					},
 				},
 			],
