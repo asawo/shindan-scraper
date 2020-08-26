36dbc7b6fe61832514858793cdd7e06ea83901d7
asawo <asawo@mercari.com>
2020-08-26 10:55:29 +0900

    condensed the dialogue block

--- index.js
+++ index.js
@@ -53,70 +53,57 @@ bot.command('/shindan', async ({ ack, say }) => {
 					type: 'section',
 					text: {
 						type: 'mrkdwn',
-						text: '*1.* ' + shindanMap[0].title + '\n`' + shindanMap[0].link + '`',
-					},
-				},
-				{
-					type: 'section',
-					text: {
-						type: 'mrkdwn',
-						text: '*2.* ' + shindanMap[1].title + '\n`' + shindanMap[1].link + '`',
-					},
-				},
-				{
-					type: 'section',
-					text: {
-						type: 'mrkdwn',
-						text: '*3.* ' + shindanMap[2].title + '\n`' + shindanMap[2].link + '`',
-					},
-				},
-				{
-					type: 'section',
-					text: {
-						type: 'mrkdwn',
-						text: '*4.* ' + shindanMap[3].title + '\n`' + shindanMap[3].link + '`',
-					},
-				},
-				{
-					type: 'section',
-					text: {
-						type: 'mrkdwn',
-						text: '*5.* ' + shindanMap[4].title + '\n`' + shindanMap[4].link + '`',
-					},
-				},
-				{
-					type: 'section',
-					text: {
-						type: 'mrkdwn',
-						text: '*6.* ' + shindanMap[5].title + '\n`' + shindanMap[5].link + '`',
-					},
-				},
-				{
-					type: 'section',
-					text: {
-						type: 'mrkdwn',
-						text: '*7.* ' + shindanMap[6].title + '\n`' + shindanMap[6].link + '`',
-					},
-				},
-				{
-					type: 'section',
-					text: {
-						type: 'mrkdwn',
-						text: '*8.* ' + shindanMap[7].title + '\n`' + shindanMap[7].link + '`',
-					},
-				},
-				{
-					type: 'section',
-					text: {
-						type: 'mrkdwn',
-						text: '*9.* ' + shindanMap[8].title + '\n`' + shindanMap[8].link + '`',
-					},
-				},
-				{
-					type: 'section',
-					text: {
-						type: 'mrkdwn',
-						text: '*10.* ' + shindanMap[9].title + '\n`' + shindanMap[9].link + '`',
+						text:
+							'*1.* ' +
+							shindanMap[0].title +
+							'\n`' +
+							shindanMap[0].link +
+							'`\n' +
+							'*2.* ' +
+							shindanMap[1].title +
+							'\n`' +
+							shindanMap[1].link +
+							'`\n' +
+							'*3.* ' +
+							shindanMap[2].title +
+							'\n`' +
+							shindanMap[2].link +
+							'`\n' +
+							'*4.* ' +
+							shindanMap[3].title +
+							'\n`' +
+							shindanMap[3].link +
+							'`\n' +
+							'*5.* ' +
+							shindanMap[4].title +
+							'\n`' +
+							shindanMap[4].link +
+							'`\n' +
+							'*6.* ' +
+							shindanMap[5].title +
+							'\n`' +
+							shindanMap[5].link +
+							'`\n' +
+							'*7.* ' +
+							shindanMap[6].title +
+							'\n`' +
+							shindanMap[6].link +
+							'`\n' +
+							'*8.* ' +
+							shindanMap[7].title +
+							'\n`' +
+							shindanMap[7].link +
+							'`\n' +
+							'*9.* ' +
+							shindanMap[8].title +
+							'\n`' +
+							shindanMap[8].link +
+							'`\n' +
+							'*10.* ' +
+							shindanMap[9].title +
+							'\n`' +
+							shindanMap[9].link +
+							'`\n',
 					},
 				},
 			],
